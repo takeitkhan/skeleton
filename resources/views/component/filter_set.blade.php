4d61887dc62cdecd708fc9f4d0ba07f5bce44d8e
takeitkhan <takeitkhan@gmail.com>
2021-03-19 13:49:02 +0600

    common search input field design done

--- resources/views/component/filter_set.blade.php
+++ resources/views/component/filter_set.blade.php
@@ -1,21 +1,27 @@
 @if(!empty($spShowFilterSet) && $spShowFilterSet == true)
 
-    <div class="level-right">
-        <div class="level-item is-5">
+    <div class="x">
+        <div class="level-right">
             <div class="control">
                 
                 {{ Form::open(array('url' => $spSearchData ?? NULL,'method' => 'get','value' => 'PATCH','id' => 'search','files' => true,'autocomplete' => 'off')) }}
-                <div class="field has-addons searchInput">
-                    <div class="control">
-                        <span class="fa fa-search form-control-feedback has-search-btn"></span>
-                        <input id="textboxID" class="input" name="key" type="text"
-                               value="{{ request()->get('key') ? request()->get('key') : ''  }}"
-                               placeholder="{{ $spPlaceholder ?? NULL }}"/>
-                    </div>
-                    <div class="control xfilterSearchBtn is-hidden-desktop">
-                        <button class="input is-small" type="submit"><i class="fas fa-search"></i></button>
-                    </div>
-                </div>
+
+                    <div class="sb-example-1">
+                        <div class="search">
+                            <input 
+                                id="textboxID" 
+                                name="key" 
+                                type="text" 
+                                class="searchTerm" 
+                                placeholder="What are you looking for?" 
+                                value="{{ request()->get('key') ? request()->get('key') : ''  }}">
+                            <button 
+                                type="submit" 
+                                class="searchButton">
+                                    <i class="fa fa-search"></i>
+                            </button>
+                        </div>
+                    </div>                
                 {{ Form::close() }}       
                 
             </div>
